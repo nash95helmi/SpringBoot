cff6d1eebb2a5ded433bbbee63069282c2d3282c
nash95helmi <54628297+nash95helmi@users.noreply.github.com>
2021-01-11 23:42:22 +0800

    Update DocumentsController.java

--- documentsManager/src/main/java/com/documentsManager/controller/DocumentsController.java
+++ documentsManager/src/main/java/com/documentsManager/controller/DocumentsController.java
@@ -33,6 +33,9 @@ public class DocumentsController {
 	@GetMapping(value = "/download/id/{id}")
 	public Optional<Documents> download(@PathVariable String id){
 		System.out.println("Testing for branching");
+		System.out.println("Testing for branching");
+		System.out.println("Testing for branching");
+		System.out.println("Testing for branching");
 		return documentsRepository.findById(id);
 	}
 	
