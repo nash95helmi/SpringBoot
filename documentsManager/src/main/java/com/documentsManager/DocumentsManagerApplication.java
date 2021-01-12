9a72499a12f641100bac646313aa4fca2a41f12a
nasar <nasar@LAPTOP-M115NNT9>
2021-01-12 14:28:50 +0800

    commit from STS

--- documentsManager/src/main/java/com/documentsManager/DocumentsManagerApplication.java
+++ documentsManager/src/main/java/com/documentsManager/DocumentsManagerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DocumentsManagerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Test hihihi");
 		SpringApplication.run(DocumentsManagerApplication.class, args);
 	}
 
